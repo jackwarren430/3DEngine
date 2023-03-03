6a94ee352d40ddb7dfc6d2df84a73b3b8d7062c5
Jack <jackwarren430@gmail.com>
2023-03-02 22:58:40 -0500

    initial commit

---
+++ 3DEngine.py
@@ -0,0 +1,46 @@
+import pygame as pg
+import numpy as np
+from Object3D import *
+from Camera import *
+from Projection import *
+
+
+class SoftwareRender:
+	def __init__(self):
+		pg.init()
+		self.RES = self.WIDTH, self.HEIGHT = 1600, 900
+		self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
+		self.FPS = 60
+		self.screen = pg.display.set_mode(self.RES)
+		self.clock = pg.time.Clock()
+		self.create_objects()
+
+
+	def create_objects(self):
+		self.camera = Camera(self, [0.5, 1, -4])
+		self.projection = Projection(self)
+		self.object = Object3D(self)
+		self.object.translate([0.2, 0.4, 0.2])
+		self.object.rotate_y(math.pi / 6)
+
+
+	def draw(self):
+		self.screen.fill(pg.Color('darkslategray'))
+		self.object.draw()
+
+	def run(self):
+		while True:
+			self.draw()
+			self.camera.control()
+			for i in pg.event.get():
+				if i.type == pg.QUIT: exit()
+			pg.display.set_caption(str(self.clock.get_fps()))
+			pg.display.flip()
+			self.clock.tick(self.FPS)
+
+if __name__ == '__main__':
+	app = SoftwareRender()
+	app.run()
+
+
+
